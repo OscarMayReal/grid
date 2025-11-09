49dd593e0a55c9751d324d1e9ea404b5cc752c00
Oscar May <oscar@quntem.co.uk>
2025-11-09 21:45:10 +0000

    add theta to the repo

---
+++ theta/config/includes.chroot_after_packages/usr/local/bin/grid-agent-launch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Only run if the system is installed
+if [[ "$USER" != "live" ]]; then
+    if [[ -f /etc/live/live-boot-marker ]]; then
+        rm -f /etc/live/live-boot-marker
+        echo "Deleted /etc/live/live-boot-marker"
+    else
+        echo "/etc/live/live-boot-marker does not exist"
+    fi
+else
+    echo "Current user is 'live', skipping deletion"
+fi
+
+if [ ! -f /etc/live/live-boot-marker ]; then
+    exec /usr/local/bin/node /grid/grid-agent/index.ts
+else 
+    exec sudo calamares
+fi
+
---
+++ theta/config/includes.chroot_after_packages/usr/local/bin/thetaos-cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Remove live-only files
+rm -rf /etc/live
+# Remove Calamares installer
+rm -rf /usr/bin/calamares /usr/share/calamares
